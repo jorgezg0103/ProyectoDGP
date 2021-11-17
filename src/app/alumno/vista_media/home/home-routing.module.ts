71c775495835ef25ec2c185a71551c5bada5271f
Jesus Lopez <jesuslr1999@correo.ugr.es>
2021-11-17 18:13:46 +0100

    plantillas cabeceras

--- src/app/alumno/vista_media/home/home-routing.module.ts
+++ src/app/alumno/vista_media/home/home-routing.module.ts
@@ -1,3 +1,8 @@
+//Nombre de archivo:home-routing.module.ts
+//Fichero encargado de ayudar al fichero de routing principal de la aplicación
+//Autor: Jesús López Rodríguez
+//Define una variable para la ruta de la página y un módulo importando y exportando las rutas utilizadas
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePage } from './home.page';
