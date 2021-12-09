d11070802911755824508b94e11df7daf6a9c567
Jesus Lopez <jlopezpeque@gmail.com>
2021-12-09 11:37:09 +0100

    Creado fichero php comandas

---
+++ src/app/back-end/comandas.php
@@ -0,0 +1,56 @@
+<?php
+require 'connection.php';
+
+function insertarPrimerPlato(){
+
+}
+
+function listaPrimerPlato(){
+
+}
+
+function leerPrimerPlato(){
+
+}
+
+function insertarSegundoPlato(){
+
+}
+
+function listaSegundoPlato(){
+
+}
+
+function leerSegundoPlato(){
+    
+}
+
+function insertarPostrePlato(){
+
+}
+
+function listaPostre(){
+
+}
+
+function leerPostre(){
+    
+}
+
+function insertarComanda(){
+
+}
+
+function listaComanda(){
+
+}
+
+function leerComanda(){
+    
+}
+
+
+
+
+
+?>
