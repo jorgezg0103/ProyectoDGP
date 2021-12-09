86c1a78482974a7a70eb98ac3f81f5214983e61c
Jesus Lopez <jlopezpeque@gmail.com>
2021-12-09 12:03:46 +0100

    comandas

--- src/app/back-end/comandas.php
+++ src/app/back-end/comandas.php
@@ -6,11 +6,61 @@ function insertarPrimerPlato(){
 }
 
 function listaPrimerPlato(){
+    $conn = createConnection();
+    $sql = "SELECT * FROM primerPlato";
+    $salida = "";
+    $result = mysqli_query($conn, $sql);
+    $lectura = "";
+    if (mysqli_query($conn, $sql)){
+        echo "\nDentro if";
+        $salida = "\nLeido con exito";
+    }
+    else {
+        echo "Dentro else";
+        $salida = "Error al insertar";
+    }
 
+    if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+        $lectura = $lectura. "\nID: " . $row["idPrimero"]. " - Name: " . $row["nombre"];
+    }
+    } else {
+    echo "0 results";
+    }
+
+    closeConnection($conn);
+    echo $salida;
+    return $lectura;
 }
 
-function leerPrimerPlato(){
+function leerPrimerPlato($idPlato){
+    $conn = createConnection();
+    $sql = "SELECT * FROM primerPlato WHERE idPrimero='".$idPlato."'";
+    $salida = "";
+    $result = mysqli_query($conn, $sql);
+    $lectura = "";
+    if (mysqli_query($conn, $sql)){
+        echo "\nDentro if";
+        $salida = "\nLeido con exito";
+    }
+    else {
+        echo "Dentro else";
+        $salida = "Error al insertar";
+    }
 
+    if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+        $lectura = $lectura. "\nID: " . $row["idPrimero"]. " - Name: " . $row["nombre"];
+    }
+    } else {
+    echo "0 results";
+    }
+
+    closeConnection($conn);
+    echo $salida;
+    return $lectura;
 }
 
 function insertarSegundoPlato(){
@@ -18,11 +68,61 @@ function insertarSegundoPlato(){
 }
 
 function listaSegundoPlato(){
+    $conn = createConnection();
+    $sql = "SELECT * FROM segundoPlato";
+    $salida = "";
+    $result = mysqli_query($conn, $sql);
+    $lectura = "";
+    if (mysqli_query($conn, $sql)){
+        echo "\nDentro if";
+        $salida = "\nLeido con exito";
+    }
+    else {
+        echo "Dentro else";
+        $salida = "Error al insertar";
+    }
 
+    if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+        $lectura = $lectura. "\nID: " . $row["idSegundo"]. " - Name: " . $row["nombre"];
+    }
+    } else {
+    echo "0 results";
+    }
+
+    closeConnection($conn);
+    echo $salida;
+    return $lectura;
 }
 
-function leerSegundoPlato(){
-    
+function leerSegundoPlato($idPlato){
+    $conn = createConnection();
+    $sql = "SELECT * FROM segundoPlato WHERE idSegundo='".$idPlato."'";
+    $salida = "";
+    $result = mysqli_query($conn, $sql);
+    $lectura = "";
+    if (mysqli_query($conn, $sql)){
+        echo "\nDentro if";
+        $salida = "\nLeido con exito";
+    }
+    else {
+        echo "Dentro else";
+        $salida = "Error al insertar";
+    }
+
+    if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+        $lectura = $lectura. "\nID: " . $row["idSegundo"]. " - Name: " . $row["nombre"];
+    }
+    } else {
+    echo "0 results";
+    }
+
+    closeConnection($conn);
+    echo $salida;
+    return $lectura;
 }
 
 function insertarPostrePlato(){
@@ -30,10 +130,62 @@ function insertarPostrePlato(){
 }
 
 function listaPostre(){
+    $conn = createConnection();
+    $sql = "SELECT * FROM postre";
+    $salida = "";
+    $result = mysqli_query($conn, $sql);
+    $lectura = "";
+    if (mysqli_query($conn, $sql)){
+        echo "\nDentro if";
+        $salida = "\nLeido con exito";
+    }
+    else {
+        echo "Dentro else";
+        $salida = "Error al insertar";
+    }
+
+    if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+        $lectura = $lectura. "\nID: " . $row["idPostre"]. " - Name: " . $row["nombre"];
+    }
+    } else {
+    echo "0 results";
+    }
+
+    closeConnection($conn);
+    echo $salida;
+    return $lectura;
 
 }
 
-function leerPostre(){
+function leerPostre($idPlato){
+    $conn = createConnection();
+    $sql = "SELECT * FROM postre WHERE idPostre='".$idPlato."'";
+    $salida = "";
+    $result = mysqli_query($conn, $sql);
+    $lectura = "";
+    if (mysqli_query($conn, $sql)){
+        echo "\nDentro if";
+        $salida = "\nLeido con exito";
+    }
+    else {
+        echo "Dentro else";
+        $salida = "Error al insertar";
+    }
+
+    if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+        $lectura = $lectura. "\nID: " . $row["idPostre"]. " - Name: " . $row["nombre"];
+    }
+    } else {
+    echo "0 results";
+    }
+
+    closeConnection($conn);
+    echo $salida;
+    return $lectura;
     
 }
 
@@ -42,7 +194,32 @@ function insertarComanda(){
 }
 
 function listaComanda(){
+    $conn = createConnection();
+    $sql = "SELECT * FROM comanda";
+    $salida = "";
+    $result = mysqli_query($conn, $sql);
+    $lectura = "";
+    if (mysqli_query($conn, $sql)){
+        echo "\nDentro if";
+        $salida = "\nLeido con exito";
+    }
+    else {
+        echo "Dentro else";
+        $salida = "Error al insertar";
+    }
 
+    if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+        $lectura = $lectura. "\nID: " . $row["idComanda"]. " - Primero: " . $row["idPrimero"]. " - Segundo: " . $row["idSegundo"]. " - Postre: " . $row["idPostre"]. " - Usuario: " . $row["usuario"];
+    }
+    } else {
+    echo "0 results";
+    }
+
+    closeConnection($conn);
+    echo $salida;
+    return $lectura;
 }
 
 function leerComanda(){
