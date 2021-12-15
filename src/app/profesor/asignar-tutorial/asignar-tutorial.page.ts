27855fb4811eb96916b1a44ac37ada62e1f7be29
Jesus Lopez <jesuslr1999@correo.ugr.es>
2021-12-15 14:18:41 +0100

    Added asignar tutorial page

--- src/app/profesor/asignar-tutorial/asignar-tutorial.page.ts
+++ src/app/profesor/asignar-tutorial/asignar-tutorial.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { HttpClient } from "@angular/common/http";
+import { Explica } from './explica_class';
 
 @Component({
   selector: 'app-asignar-tutorial',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AsignarTutorialPage implements OnInit {
 
-  constructor() { }
+  explica = new Explica(undefined,undefined);
+  constructor(public navCtrl: NavController, private http:HttpClient) {   
+  }
 
   ngOnInit() {
+  }
+
+  public aniadir_tutorial(){
+    this.http.get("http://localhost/tareas.php?opcion=4&idTarea="+this.explica.idTarea+"&idTutorial="+this.explica.idTutorial).subscribe(snap => {
+      console.log(snap);
+    });
   }
 
 }
