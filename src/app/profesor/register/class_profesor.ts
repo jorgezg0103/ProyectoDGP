6378fc2d94d1bf81c1daf8d41602fb039a0b0294
Ignvcix17 <55834852+Ignvcix17@users.noreply.github.com>
2021-12-15 13:26:33 +0100

    Añado registro e inicio sesion profesor

---
+++ src/app/profesor/register/class_profesor.ts
@@ -0,0 +1,9 @@
+export class Profesor {
+    constructor(
+        public usuario: string,
+        public dni: string,
+        public nombre: string,
+        public apellidos: string,
+        public email: string,
+    ) { }
+}
